cf2489db51f561309755bbce050f513695b8eac8
eliasjuk <eliasp.juk@hotmail.com>
2020-04-22 15:51:56 -0300

    :space_invader: Categories

--- spotcode/app/controllers/api/v1/dashboard_controller.rb
+++ spotcode/app/controllers/api/v1/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::DashboardController < ApplicationController
+end
+class Api::V1::DashboardController < ApplicationController
+
     def index
         load_recent_heard
+        load_recommendations
     end
 
     private
@@ -10,6 +14,12 @@ class Api::V1::DashboardController < ApplicationController
     end
 
     def load_recommendations
-        heard_categories = @recent_albums
+        heard_categories = @recent_albums.map(&:category)
+        if heard_categories.present?
+            @recommend_albums = Album.joins(:category, :songs).where(category: heard_categories).order('songs.played_count')
+                                                              .select('distinct albums.*').limit(12)
+        else
+            @recommend_albums = Album.all.sample(12)
+        end
     end
 end
