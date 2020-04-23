1748a5ba990e628de38c317cf532a34c3b64dbbd
eliasjuk <eliasp.juk@hotmail.com>
2020-04-23 17:43:39 -0300

    Import and configure Bulma

---
+++ spotcode/app/javascript/screens/discovery/index.js
@@ -0,0 +1,11 @@
+import React, { fragment, Fragment } from 'react';
+
+const DiscoveryScreen = () => {
+    return(
+        <Fragment>
+            <div>Discovery Screen</div>
+        </Fragment>
+    )
+}
+
+export default DiscoveryScreen;
---
+++ spotcode/app/javascript/screens/home/index.js
@@ -0,0 +1,11 @@
+import React, { fragment, Fragment } from 'react';
+
+const HomeScreen = () => {
+    return(
+        <Fragment>
+            <div>Home Screen</div>
+        </Fragment>
+    )
+}
+
+export default HomeScreen
