1c66cffa796c1bc2cd792c4dc8aaf9762da50987
eliasjuk <eliasp.juk@hotmail.com>
2020-04-25 15:20:24 -0300

    :notes: Carregar albums

--- spotcode/app/javascript/screens/discovery/index.js
+++ spotcode/app/javascript/screens/discovery/index.js
@@ -1,12 +1,17 @@
 import React, { Fragment } from 'react';
-import NavbarFooter from '../../components/common/navbar_footer'
+import NavbarFooter from '../../components/common/navbar_footer';
+import Discovery from '../../components/common/discovery';
+import SectionWrapper from '../../components/common/section_wrapper';
 
 const DiscoveryScreen = () => {
-    return(
-        <Fragment>
-            <div>Discovery Screen</div>
-        </Fragment>
-    )
+  return(
+    <Fragment>
+      <SectionWrapper>
+        <Discovery />
+      </SectionWrapper>
+      <NavbarFooter />
+    </Fragment>
+  )
 }
 
 export default DiscoveryScreen;
