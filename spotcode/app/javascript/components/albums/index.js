28fa4f1288afb6701cf251fba95371c79cfe74d8
eliasjuk <eliasp.juk@hotmail.com>
2020-04-26 11:15:28 -0300

    :sound: Musics

--- spotcode/app/javascript/components/albums/index.js
+++ spotcode/app/javascript/components/albums/index.js
@@ -1,21 +1,35 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { Heading, Columns, Image } from 'react-bulma-components';
 import styled from 'styled-components';
+import AlbumsService from '../../services/albums'
+import { useParams } from 'react-router-dom'
 
 const DivVSpaced = styled.div`
-  marging-top: 20px;
+  margin-top: 20px;
   margin-bottom: 20px;
 `
 
 const Albums = () => {
+  let { id } = useParams();
+  const [album, setAlbum] = useState([]);
+
+  async function fetchAlbum() {
+    const response = await AlbumsService.show(id);
+    setAlbum(response.data);
+  }
+
+  useEffect(() => {
+    fetchAlbum();
+  }, [])
+
   return(
     <Fragment>
-      <Columns>
-        <Columns.Column>
-          <Image src='' />
+      <Columns className='is-vcentered is-mobile is-centered'>
+        <Columns.Column desktop={{size: 3}} className='has-text-centered'>
+          <Image src={album.cover_url} />
           <DivVSpaced>
-            <Heading>Título</Heading>
-            <Heading>SubTitulo</Heading>
+            <Heading size={5} className='has-text-white'>{album.title}</Heading>
+            <Heading size={6} subtitle className='has-text-white'>{album.artist_name}</Heading>
           </DivVSpaced>
         </Columns.Column>
       </Columns>   
